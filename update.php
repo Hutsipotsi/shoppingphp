f6c9e8e1b79ee835a482470fae39752996701439
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-02-23 19:29:06 +0200

    pieni korjaus

--- update.php
+++ update.php
@@ -5,15 +5,16 @@ require_once 'inc/headers.php';
 $input = json_decode(file_get_contents('php://input'));
 $id = filter_var($input->id,FILTER_SANITIZE_NUMBER_INT);
 $description = filter_var($input->description,
-FILTER_SANITIZE_STRING);
+FILTER_SANITIZE_SPECIAL_CHARS);
 $amount = filter_var($input->amount,FILTER_SANITIZE_NUMBER_INT);
 
 try {
     $db = openDb();
     
-    $query = $db->prepare('update task set description=:description, amount=:amount where id=:id');
-    $query->bindValue(':description',$description,PDO::PARAM_STR);
+    $query = $db->prepare('update item set description=:description, amount=:amount where id=:id');
     $query->bindValue(':id',$id,PDO::PARAM_INT);
+    $query->bindValue(':description',$description,PDO::PARAM_STR);
+    $query->bindValue(':amount',$amount,PDO::PARAM_INT);
     $query->execute();
     
     header('HTTP/1.1 200 OK');
